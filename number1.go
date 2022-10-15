ed7b34bbd2f10e37e21870f36808d062952fe9d8
aliesmailpour <ali_morgan80@yahooo.com>
2022-10-16 00:03:04 +0330

    first commit; adds gitignore

---
+++ number1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// write a program that prints a number in decimal, binary, and hex
+
+func main(){
+
+	x:= 989
+
+	
+	fmt.Printf("%d\n" , x)
+	fmt.Printf("%b\n" , x)
+	fmt.Printf("%#x\n" , x)
+	
+
+
+
+}
