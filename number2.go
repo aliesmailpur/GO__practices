a356772d9abbb839770dcd67887d52de1efa49e6
aliesmailpour <ali_morgan80@yahooo.com>
2022-10-16 14:13:39 +0330

    number2.go

---
+++ number2.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+
+
+// using the following operators, write expressions and 
+// assign their values to variables
+// ==
+// <=
+// >=
+// !=
+// <
+// >
+
+func main(){
+
+	a:= (24 == 54)
+	b:= (55 <= 45)
+	c:= (88 >= 78)
+	d:= (23 != 23)
+	e:= (23 < 55)
+	s:= (99 > 83)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(s)
+	
+
+
+
+
+}
